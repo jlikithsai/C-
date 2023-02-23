bb94506977e0bda8f6c53cae58f138c322380b91
jlikithsai <117892485+jlikithsai@users.noreply.github.com>
2023-02-23 11:38:19 +0530

    Create functions.cpp
    This contains basic function

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void likith(){
+  cout << "I am Likith Sai Jonna" << endl ;
+}
+// We use a function to do the same operation numerous number of times inside the main function
+int main(){
+int i,n ;
+  cin >> n ;
+  for(i=0;i<n;i++){
+    likith() ;
+    // Here we are using the given function likith which we had declared at the start 
+  }
+  
+  
