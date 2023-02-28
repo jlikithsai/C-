37a553801aeb77472b76dc832d46adc6d6d83daa
jlikithsai <117892485+jlikithsai@users.noreply.github.com>
2023-02-28 19:43:52 +0530

    Create Defaultfunc.cpp

---
+++ Defaultfunc.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void temp(int =10,float = 5.5) ;
+int main(){
+int a =5 ;
+float b = 2.2 ;
+temp();
+  // The default temp will run 
+temp(a);
+  // the temp with integer as a and float as defaut will run
+temp(a,b);
+  // The temp with a,b will run 
+}
+void temp(int c ; float d ){
+  for(int i =0; i <c ; i++){
+    cout << b << "," ;
+  }
+  cout << endl ;
+}
